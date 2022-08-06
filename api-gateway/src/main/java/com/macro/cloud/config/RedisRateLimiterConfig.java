3f861576a1293498f236ee6a162b3a3f82e7389b
443309736@qq.com <zbydln2018>
2022-08-06 19:08:55 +0800

    Spring Cloud Gateway：新一代API网关服务

---
+++ api-gateway/src/main/java/com/macro/cloud/config/RedisRateLimiterConfig.java
@@ -0,0 +1,23 @@
+package com.macro.cloud.config;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author 月夜的星空
+ * @date 2022/8/6 19:04
+ */
+@Configuration
+public class RedisRateLimiterConfig {
+//    @Bean
+//    KeyResolver userKeyResolver() {
+//        return exchange -> Mono.just(exchange.getRequest().getQueryParams().getFirst("username"));
+//    }
+
+    @Bean
+    public KeyResolver ipKeyResolver() {
+        return exchange -> Mono.just(exchange.getRequest().getRemoteAddress().getHostName());
+    }
+}
---
+++ api-gateway/src/main/java/com/macro/cloud/controller/FallbackController.java
@@ -0,0 +1,24 @@
+package com.macro.cloud.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author 月夜的星空
+ * @date 2022/8/6 19:05
+ */
+@RestController
+public class FallbackController {
+
+    @GetMapping("/fallback")
+    public Object fallback() {
+        Map<String,Object> result = new HashMap<>();
+        result.put("data",null);
+        result.put("message","Get request fallback!");
+        result.put("code",500);
+        return result;
+    }
+}
